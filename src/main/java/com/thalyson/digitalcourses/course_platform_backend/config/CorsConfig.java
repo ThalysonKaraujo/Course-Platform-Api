ac49b70265e9a3cff54657cfdf4bdf903a7b62d5
ThalysonKaraujo <thalyson.kauan7@gmail.com>
2025-08-10 18:53:33 -0300

    teste

--- src/main/java/com/thalyson/digitalcourses/course_platform_backend/config/CorsConfig.java
+++ src/main/java/com/thalyson/digitalcourses/course_platform_backend/config/CorsConfig.java
@@ -10,10 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:8000",
-                        "https://course-platform-api-production.up.railway.app"
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
