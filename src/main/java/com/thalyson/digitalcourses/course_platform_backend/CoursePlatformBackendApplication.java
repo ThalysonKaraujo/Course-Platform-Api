62941c7e418ff3c20981707a139d866d7233cbbb
ThalysonKaraujo <thalyson.kauan7@gmail.com>
2025-08-10 19:30:41 -0300

    testee

--- src/main/java/com/thalyson/digitalcourses/course_platform_backend/CoursePlatformBackendApplication.java
+++ src/main/java/com/thalyson/digitalcourses/course_platform_backend/CoursePlatformBackendApplication.java
@@ -1,8 +1,11 @@
 package com.thalyson.digitalcourses.course_platform_backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://course-platform-api-production.up.railway.app", description = "Server de Produção")})
 @SpringBootApplication
 public class CoursePlatformBackendApplication {
 
