62a8947b2acc42a9be5489d1f3cfbea72f7f7023
ThalysonKaraujo <thalyson.kauan7@gmail.com>
2025-08-10 18:58:54 -0300

    Continuando testes

--- src/main/java/com/thalyson/digitalcourses/course_platform_backend/config/CorsConfig.java
+++ src/main/java/com/thalyson/digitalcourses/course_platform_backend/config/CorsConfig.java
@@ -1,17 +1,23 @@
 package com.thalyson.digitalcourses.course_platform_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permite todas as origens
+                        .allowedMethods("*") // Permite todos os métodos
+                        .allowedHeaders("*"); // Permite todos os headers
+            }
+        };
     }
 }
